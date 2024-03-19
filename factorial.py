fe704c3586eeb0b9044a5984ded04437ad0dca70
coderjahid <jahidul5035@gmail.com>
2024-03-19 12:36:09 +0600

    first commit

---
+++ factorial.py
@@ -0,0 +1,17 @@
+# factorial
+
+f=1
+a=int(input("Write a number"))
+
+if a>0:
+    print("factorial exist")
+
+elif a==0:
+        print("Factorial is 1")
+
+        
+
+
+
+
+
---
+++ hellloworld.py
@@ -0,0 +1,22 @@
+
+print("Hello World")
+
+# exercise 1
+
+First_Name ="Jahid"
+Last_name="Hasan"
+
+age="23 years old"
+
+is_tall = True
+
+print(First_Name+""+Last_name)
+
+print(age)
+print(is_tall)
+
+# User input
+Name=input("What is your Name")
+
+print(Name)
+print("Hello" + Name)
